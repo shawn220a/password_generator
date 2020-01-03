9a27575fab81a9b1eb95ea67972a060ad9e1f9c4
shawn220a <shawn.tschoepe@icloud.com>
2020-01-03 15:00:13 -0600

    Add arrays for password generator

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const numberChar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+
 
 // Write password to the #password input
 function writePassword() {
