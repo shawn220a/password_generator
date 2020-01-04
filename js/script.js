5c499ba27247d713c27813a89e51c83e593c456a
shawn220a <shawn.tschoepe@icloud.com>
2020-01-03 21:30:23 -0600

    Fixed syntax

--- js/script.js
+++ js/script.js
@@ -14,9 +14,9 @@ function generatePassword() {
 
   let passLength = prompt('How long do you want the password?');
   if (passLength < 8) {
-    alert('Password must be greater than 8 characters.')
+    alert('Password must be greater than 8 characters.');
   } else if (passLength > 128) {
-    alert('Password must be less than 128 characters.')
+    alert('Password must be less than 128 characters.');
   }
 
   let lower = confirm('Would you like lower case characters?');
