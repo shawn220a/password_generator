a77ac4431dac5cae6ad2cb39480d55da07d79467
shawn220a <shawn.tschoepe@icloud.com>
2020-01-05 00:21:55 -0600

    Add full special characters to array

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 const lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numberChar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+const specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "\"", "'", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "[", "]", "\\", "_", "`", "{", "}", "|", "~", " "];
 
 // Generate Password
 function generatePassword() {
