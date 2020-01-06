41fa26fc53177673acf72542d44ff0b14abe81f4
shawn220a <shawn.tschoepe@icloud.com>
2020-01-05 19:28:21 -0600

    Create Functions to clean up code

--- js/script.js
+++ js/script.js
@@ -6,53 +6,63 @@ const upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L
 const numberChar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "\"", "'", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "[", "]", "\\", "_", "`", "{", "}", "|", "~", " "];
 
+let password = [];
+let passwordChars = [];
+let passwordText = "";
+
+function userInput() {
+  let lower = confirm("Would you like lower case characters?");
+  let upper = confirm("Would you like upper case characters?");
+  let number = confirm("Would you like numbers?");
+  let special = confirm("Would you like special case characters?");
+
+  if (lower === true) {
+    for (x of lowerCaseChar) {
+      password.push(x);
+    }
+  }
+  if (upper === true) {
+    for (x of upperCaseChar) {
+      password.push(x);
+    }
+  }
+  if (number === true) {
+    for (x of numberChar) {
+      password.push(x);
+    }
+  }
+  if (special === true) {
+    for (x of specialChar) {
+      password.push(x);
+    }
+  }
+  return password;
+}
+
+function randomizePassword(passLength, password) {
+  for (i = 0; i < passLength; i++) {
+    let passwordChoice = Math.floor(Math.random() * password.length);
+    let passChar = password[passwordChoice];
+    passwordChars.push(passChar);
+  }
+
+  for (x of passwordChars) {
+    passwordText += x;
+  }
+  return passwordText;
+}
+
 // Generate Password
 function generatePassword() {
-  let password = [];
-  let passwordChars = [];
-  let passwordText = "";
-
   let passLength = prompt("How long do you want the password?");
   if (passLength < 8) {
     alert("Password must be greater than 8 characters.");
   } else if (passLength > 128) {
     alert("Password must be less than 128 characters.");
   } else {
-    let lower = confirm("Would you like lower case characters?");
-    let upper = confirm("Would you like upper case characters?");
-    let number = confirm("Would you like numbers?");
-    let special = confirm("Would you like special case characters?");
 
-    if (lower === true) {
-      for (x of lowerCaseChar) {
-        password.push(x);
-      }
-    }
-    if (upper === true) {
-      for (x of upperCaseChar) {
-        password.push(x);
-      }
-    }
-    if (number === true) {
-      for (x of numberChar) {
-        password.push(x);
-      }
-    }
-    if (special === true) {
-      for (x of specialChar) {
-        password.push(x);
-      }
-    }
-
-    for (i = 0; i < passLength; i++) {
-      let passwordChoice = Math.floor(Math.random() * password.length);
-      let passChar = password[passwordChoice];
-      passwordChars.push(passChar);
-    }
-
-    for (x of passwordChars) {
-      passwordText += x;
-    }
+    password = userInput();
+    passwordText = randomizePassword(passLength, password);
 
     writePassword(passwordText);
   }
